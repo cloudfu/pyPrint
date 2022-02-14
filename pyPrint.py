b9f84e42181c8835345282163375a016b4288fd9
pi667i <hua.fu@softlinker.com>
2022-02-14 21:14:25 +0800

    change

--- pyPrint.py
+++ pyPrint.py
@@ -8,7 +8,7 @@ from texttable import Texttable
 # 说明：飞鹅云后台配置参数
 URL = "http://api.feieyun.cn/Api/Open/"  # 不需要修改
 USER = "1055387938@qq.com"  # *必填*：飞鹅云后台注册账号
-UKEY = "HPJtdVP3FZSBnkJ2"  # *必填*: 飞鹅云后台注册账号后生成的UKEY 【备注：这不是填打印机的KEY】
+UKEY = "PTHPJtdVP3FZSBnkJ2"  # *必填*: 飞鹅云后台注册账号后生成的UKEY 【备注：这不是填打印机的KEY】
 SN = "15960800945"  # *必填*：打印机编号，必须要在管理后台里手动添加打印机或者通过API添加之后，才能调用API
 
 
